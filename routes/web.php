c4b1d3a50a3f7baae9f8655ab85b27e00f0882de
Diego Morales Hernandez <die.moralesh@gmail.com>
2018-01-24 22:18:52 -0300

    actulizacion de rutas

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,6 @@ Route::redirect('/', 'blog');
 Auth::routes();
 
 Route::get('blog', 'Web\PageController@blog')->name('blog');
-
-Route::get('blog/{slug}', 'Web\PageController@post')->name('post');
+Route::get('entrada/{slug}', 'Web\PageController@post')->name('post');
+Route::get('categoria/{slug}', 'Web\PageController@category')->name('category');
+Route::get('etiqueta/{slug}', 'Web\PageController@tag')->name('tag');
